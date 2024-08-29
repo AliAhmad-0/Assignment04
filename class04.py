336921edc1bdb9261090e94753f691e8f9e0f728
AliAhmad-0 <aliahmad554327@gmail.com>
2024-08-29 13:08:37 +0500

    built in or userDef function

---
+++ class04.py
@@ -0,0 +1,200 @@
+Write a program that calculates the area of the cube.(builtin function)
+
+def calculateAreaCube(side_length):
+    area = 6 * side_length**2
+    return area
+area = calculateAreaCube(5)
+print(area)
+
+By User def function
+def calculate_area():
+    side_length = int(input("enter the side_length:"))
+    area = 6 * side_length **2
+    return area
+cubeArea = calculate_area()
+print(cubeArea)
+
+
+
+
+Write a program that calculates the area of a circle.
+
+def CalculateAreaOfCircle(pi,r):
+    area = 3.14 * r**2
+    return area
+area = CalculateAreaOfCircle(3.14,5)
+print(area)
+
+By user def function
+def areaOFCircle():
+    radius = int(input("enter the Area of radius:"))
+    C = 3.14 * radius**2
+    return C
+circleArea = areaOFCircle()
+print(circleArea)
+
+
+
+Calculate your age based on the current year and your birth year.
+
+def calculateAge(currentYear,BirthYear):
+    Age = 2024 - 1999
+    return Age
+totalAge = calculateAge(2024,1999)
+print(totalAge)
+
+By Userdef function
+def calculateAge():
+    currentYear = int(input("enter the currentyear:"))
+    BirthYear = int (input("enter the BirthYear:"))
+    Age = currentYear - BirthYear
+    return Age
+finalAge = calculateAge()
+print(finalAge)
+
+
+
+Write a program that calculates the area of a rectangle
+using length and width variables.
+
+def calculateAreaRectangle(length,width):
+    Area = length*width
+    return Area
+totalrectangle = calculateAreaRectangle(5,6)
+print(totalrectangle)
+
+By UserDef function
+def calculateArea():
+    length = int(input("enter the value of length:"))
+    width = int(input("enter the value of width:"))
+    Area = length * width
+    return Area
+RectangleArea = calculateArea()
+print(RectangleArea)
+
+
+
+ Create a program that converts a temperature from
+Fahrenheit to Celsius and vice versa using a variable.
+
+def convertTemperature(fahrenheit):
+    Celsius = (40 - 32) * 5/9
+    return Celsius
+temperature =convertTemperature(40)
+print(temperature)
+
+By UserDef function
+def fahrenheitToCelsius():
+    F = int(input("enter the value of Fahrenheit:"))
+    Celsius = (F - 32) * 5/9
+    return Celsius
+temerature = fahrenheitToCelsius()
+print(temerature)
+
+
+
+ Create a program that converts a temperature from 
+ Celsius to fahrenheit and vice versa using a variable.
+
+def convertTemperature(celsius):
+    fahrenheit = 40 * (9/5) + 32
+    return fahrenheit
+temperature = convertTemperature(40)
+print(temperature)
+
+By userDef function
+def celsiusToFahrenheit():
+    c = int(input("enter the value of Celsius:"))
+    Formula = (c * 9/5) + 32
+    return Formula
+Temperature = celsiusToFahrenheit()
+print(Temperature)
+    
+
+
+
+ Convert a given number of seconds into minutes and seconds using variables.
+
+def convertSecond(second):
+    minutes = 45 / 60
+    return minutes
+result= convertSecond(45)
+print(result) 
+
+By userDef function
+def calculate_second_to_minutes():
+    S = int(input("enter the value of second:"))
+    formula = S / 60
+    return formula
+calculateMinute = calculate_second_to_minutes()
+print(calculateMinute)
+
+
+
+Write a program that calculates the percentage.
+
+def calculate_percentage(value):
+    percentage = (75/130) * 100
+    return percentage
+total= calculate_percentage(75)
+print(total)
+
+By userDef function
+def calculate_percentage():
+    Value = int(input("enter the value:"))
+    formula = ( Value / 130 ) * 100
+    return formula
+percentageCalculate = calculate_percentage()
+print(percentageCalculate)
+
+
+
+
+Write a program that calculates the BMI using 
+height (in meters) and weight (in kilograms) variables;
+
+def calculate_BMI(weight,height):
+    BMI = 80 / 3**2  
+    return BMI
+calculated = calculate_BMI(80,3)
+print(calculated)
+
+By userDef function;
+
+ def calculate_BMI():
+    weight = int(input("enter the value of weight:"))
+    height = int(input("enter the value of height:"))
+    BMI = (weight / height**2)
+    return BMI
+bmiCalculate = calculate_BMI()
+print(bmiCalculate)
+
+
+
+Write a program that calculates the volume of a cylinder using the formula 
+
+def calculate_volumeOfCylinder(Pi,r,h):
+    v = (Pi)*(r**2)*(h)
+    return v
+cylinderVolume = calculate_volumeOfCylinder(3.14,5,8)
+print(cylinderVolume)
+    
+By userDef function
+def calculate_volumeOfCyclinder():
+    r = int(input("enter the value of r:"))
+    h = int(input("enter the value of h:"))
+    v = 3.14 * (r**2) * (h)
+    return v
+cyclinderVolume = calculate_volumeOfCyclinder()
+print(cyclinderVolume)
+
+
+
+
+
+
+ 
+    
+    
+  
+
